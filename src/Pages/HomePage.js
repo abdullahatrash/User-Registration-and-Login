05eab6147875e22d479683f71d77170acee662fc
unknown <abodi1987@gmail.com>
2021-05-10 20:53:51 +0300

    Setup the prject and working env

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div className="">
+            <h1>Hi Abdullah!</h1>
+            <p>You're logged in with React!!</p>
+            <h3>All registered users:</h3>
+        </div>
+    )
+}
+
+export default HomePage
