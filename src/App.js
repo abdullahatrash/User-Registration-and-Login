7d1611a3f530bb023ced7c44cabafb619a310ffa
unknown <abodi1987@gmail.com>
2021-05-10 21:52:48 +0300

    added tailwind to the project and created the home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import "tailwindcss/tailwind.css"
 import './App.css';
 import HomePage from "./Pages/HomePage";
 
+
+
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 function App() {
   return (
+    <Router>
     <div>
+      <Switch>
+      <Route path="/">
      <HomePage/>
+     </Route>
+     </Switch>
     </div>
+    </Router>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+   
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      },
+    },
   },
   variants: {
     extend: {},
